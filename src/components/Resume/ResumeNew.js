f18e20bca0e3461501547c9b14283e9bc398a7ff
Rushia.DeV <97021842+RushiaDeV@users.noreply.github.com>
2022-09-13 20:29:43 +0200

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-    "https://raw.githubusercontent.com/Rushia.DeV/portfolio/master/src/Assets/EmilianTreek_BKO.pdf";
+    "https://raw.githubusercontent.com/RushiaDeV/Portfolio/main/src/Assets/EmilianTreek_BKO.pdf";
 
 function ResumeNew() {
     const [width, setWidth] = useState(1200);
